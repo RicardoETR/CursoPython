a61d7f3f6a532e8edffaa0085ca3a55e5a45351e
Ricardo Erick Torres Rosas <r.erick.torres.r@gmail.com>
2023-09-19 00:24:54 -0600

    add operations_set

---
+++ operations_set.py
@@ -0,0 +1,42 @@
+
+set_a = {'col', 'mex', 'bol'}
+set_b = {'pe', 'bol'}
+
+# UNION
+#metodo
+set_c = set_a.union(set_b)
+print(set_c)
+#operador
+set_c = (set_a | set_b)
+print(set_c)
+
+
+#INTERSECCION
+#metodo
+set_c = set_a.intersection(set_b)
+print(set_c)
+#operador
+set_c = (set_a&set_b)
+print(set_c)
+
+#DIFERENCIA
+set_c = set_a.difference(set_b)
+print(set_c)
+set_c = (set_a - set_b)
+print(set_c)
+
+#DIFERENCIA SIMETRICA (UNION SIN ELEMENTOS QUE CONCIDEN)
+set_c = set_a.symmetric_difference(set_b)
+print(set_c)
+set_c = set_a ^ set_b
+print(set_c)
+
+
+#ejercicio de union
+countries = {'mex', 'col', 'arg', 'usa'}
+nothAm = {'usa', 'canada'}
+centralAm = {'mex', 'gt', 'bz'}
+southAm = {'col', 'bz', 'arg'}
+
+new_set = countries.union(nothAm, centralAm, southAm)
+print(new_set)
