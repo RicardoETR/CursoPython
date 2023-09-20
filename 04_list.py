0e6d912b9343b8d09194944cd143defdd77373b2
Ricardo Torres <r.erick.torres.r@gmail.com>
2023-09-19 23:05:56 -0600

    arreglando problema en los nombres

--- 04_list.py
+++ 04_list.py
@@ -1,10 +1,9 @@
+#lista normal
 numbers = []
-
 for element in range(1,11):
     if element % 2 == 0:
         numbers.append(element * 2)
 print(numbers)
-
 
 # list comprehesion
 numbers_v2 = [element * 2 for element in range(1,11) if element % 2 == 0 ]
